a32346e58a0b1f74844b8f4f5b6bdccb5ec301ff
mchd921 <smachida921@gmail.com>
2024-12-02 09:35:09 +0900

    add restAPI

--- src/main/java/petclinic_clone/dev/controller/OwnerController.java
+++ src/main/java/petclinic_clone/dev/controller/OwnerController.java
@@ -3,25 +3,22 @@ package petclinic_clone.dev.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import petclinic_clone.dev.model.Owner;
 import petclinic_clone.dev.service.OwnerService;
 
-@Controller
+@RestController
 public class OwnerController {
 
     @Autowired
     OwnerService service;
 
     @GetMapping("/owner-list")
-    public String ownerList(Model model) {
-
-        List<Owner> ownerList = service.findAll();
-        model.addAttribute("ownerList", ownerList);
-        return "ownerList";
+    public List<Owner> ownerList(Model model) {
+        return service.findAll();
     }
     
 }
